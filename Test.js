50aecd715e36103a2608ad705ab0f7b7b07ab107
sunwenzhangdan <189802750@qq.com>
2016-07-16 12:35:42 +0800

    second

--- Test.js
+++ Test.js
@@ -1,4 +1,17 @@
-var async = require('async');
-async.series({},function(err, results) {
-	console.log(results);
+var fs = require('fs')
+filenames = fs.readdirSync(".");
+for (i = 0; i < filenames.length; i++) {
+    console.log(filenames[i]);
+}
+console.log('Current uid: ' + process.getuid());
+
+console.log("++++++++++++++++++++++++")
+
+fs.readdir(".", function(err, m) {
+    console.log(m);
+    var i;
+    for (i = 0; i < m.length; i++) {
+        console.log(m[i]);
+    }
 });
+console.log('Current uid: ' + process.getuid());
